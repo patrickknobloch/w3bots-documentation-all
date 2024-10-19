d81f7e7d6273171ab3078be7d021674cbf780319
patrickknobloch <115040494+patrickknobloch@users.noreply.github.com>
2024-10-19 22:56:43 +0200

    new changes in docs

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -35,10 +35,6 @@ const config: Config = {
       {
         docs: {
           sidebarPath: './sidebars.ts',
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
         blog: {
           showReadingTime: true,
@@ -47,10 +43,7 @@ const config: Config = {
             xslt: true,
           },
           // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-          // Useful options to enforce blogging best practices
+          
           onInlineTags: 'warn',
           onInlineAuthors: 'warn',
           onUntruncatedBlogPosts: 'warn',
@@ -82,7 +75,8 @@ const config: Config = {
         
       ],
     },
-   
+    docs: {
+    },
     colorMode: {
       defaultMode: 'dark',
       disableSwitch: true, // Deaktiviert den Light/Dark-Mode-Button
