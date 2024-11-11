790ad03f39c26d612cc5272d20cd90ce6d43ae34
patrickknobloch <115040494+patrickknobloch@users.noreply.github.com>
2024-11-11 21:59:32 +0100

    header hover button

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -39,11 +39,11 @@ function HomepageHeader() {
 
     <div className="mt-8 gap-3 flex justify-center">
     <a href="#_" className="relative inline-flex items-center px-12 py-3 overflow-hidden text-lg font-medium text-white border-2 border-neutral-700 rounded-full hover:text-neutral-900 hover:no-underline group ">
-<span className="absolute left-0 block w-full h-0 transition-all bg-white opacity-100 group-hover:h-full top-1/2 group-hover:top-0 duration-400 ease"></span>
-<span className="absolute right-0 flex items-center justify-start w-10 h-10 duration-300 transform translate-x-full group-hover:translate-x-0 ease group:hover:text-neutral-900">
+<span className="absolute left-0 block w-full h-0 transition-all bg-white opacity-100 group-hover:h-full top-1/2 group:hover:text-neutral-900 hover:text-neutral-900 group-hover:top-0 duration-400 ease"></span>
+<span className="absolute right-0 flex items-center justify-start w-10 h-10 duration-300 transform translate-x-full hover:text-neutral-900 group-hover:translate-x-0 ease group:hover:text-neutral-900">
 <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14 5l7 7m0 0l-7 7m7-7H3"></path></svg>
 </span>
-<span className="relative">Discover our Products</span>
+<span className="relative hover:text-neutral-900">Discover our Products</span>
 </a>
     </div>
   </div>
