6f34a752422e48bf69e2e4bb5a78feadc64216bc
patrickknobloch <115040494+patrickknobloch@users.noreply.github.com>
2024-11-22 09:00:23 +0100

    new waitlist text and link to jotforms

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -227,7 +227,7 @@ function Products() {
           </div>
         </div>
       
-       
+      
       </div>
     </div>
   );
@@ -272,18 +272,18 @@ function JoinWaitlist(){
   return(
     <div className="">
       <div className="mx-auto py-12 sm:px-6 sm:py-12">
-        <div className="mx-4 rounded-xl relative isolate overflow-hidden p-8 border border-neutral-800/30 bg-neutral-900/5 bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-blue-500/10 via-blue-300/0  to-green-300/0  shadow-2xl sm:rounded-3xl ">
+        <div className="mx-4 rounded-xl relative isolate overflow-hidden p-8 border border-neutral-800/30 bg-neutral-900/5 bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-blue-300/10 via-blue-300/0  to-green-300/0  shadow-2xl sm:rounded-3xl ">
     
         <div className='backdrop-blur-sm relative z-30 w-full flex p-4 rounded-xl'>
             <div className="mx-auto text-center lg:py-16 ">
-              <h2 className="text-5xl font-bold tracking-tight text-white sm:text-8xl">
-                Join Waitlist now!
+              <h2 className="text-5xl font-bold tracking-tight text-white sm:text-6xl">
+              Sign Up for Early Access!
               </h2>
               <p className="mt-6 text-lg leading-8 text-neutral-300">
-              Join discussions on token insights, exchange ideas, and stay updated on our latest updates.
+              Stay informed with our latest updates.              
               </p>
              
-              <a href="#products" className="mt-12 hover:shadow-xl hover:shadow-white/5 relative inline-flex items-center px-12 py-3 overflow-hidden text-lg font-medium text-white border border-neutral-700 rounded-full hover:text-neutral-900 hover:no-underline group ">
+              <a href="https://form.jotform.com/MTmt_mtMTmt/w3bots-product-updates-sign-up" className="mt-12 hover:shadow-xl hover:shadow-white/5 relative inline-flex items-center px-12 py-3 overflow-hidden text-lg font-medium text-white border border-neutral-700 rounded-full hover:text-neutral-900 hover:no-underline group ">
                 <span className="absolute left-0 block w-full h-0 transition-all bg-white/10 opacity-100 group-hover:h-full top-1/2  group-hover:top-0 duration-400 ease"></span>
                 <span className="absolute right-0 flex items-center justify-start w-10 h-10 duration-300 transform translate-x-full group-hover:translate-x-0 ease group:hover:text-neutral-100">
                 <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14 5l7 7m0 0l-7 7m7-7H3"></path></svg>
