3cc19c43798aa2bceb41a5cdfa22e8a9930f9a3f
Pedro Augusto <16762743+pedronaugusto@users.noreply.github.com>
2024-11-25 17:39:01 +0000

    changes telegram links

--- src/pages/custom-services.tsx
+++ src/pages/custom-services.tsx
@@ -39,7 +39,7 @@ function HomepageHeader() {
     </div>
 
     <div className="mt-8 gap-3 flex justify-center">
-      <a href="#products" className="hover:shadow-xl hover:shadow-white/5 relative inline-flex items-center px-12 py-3 overflow-hidden text-lg font-medium text-white border border-neutral-700 rounded-full hover:text-neutral-900 hover:no-underline group ">
+      <a href="https://t.me/+t1qAnJclybgwODQy" className="hover:shadow-xl hover:shadow-white/5 relative inline-flex items-center px-12 py-3 overflow-hidden text-lg font-medium text-white border border-neutral-700 rounded-full hover:text-neutral-900 hover:no-underline group ">
         <span className="absolute left-0 block w-full h-0 transition-all bg-white/10 opacity-100 group-hover:h-full top-1/2  group-hover:top-0 duration-400 ease"></span>
         <span className="absolute right-0 flex items-center justify-start w-10 h-10 duration-300 transform translate-x-full group-hover:translate-x-0 ease group:hover:text-neutral-100">
         <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14 5l7 7m0 0l-7 7m7-7H3"></path></svg>
@@ -286,7 +286,7 @@ function Community(){
               </p>
               <div className="mt-10 flex items-center justify-center gap-x-6">
                 <a
-                  href="https://t.me/w3bots_community"
+                  href="https://t.me/+t1qAnJclybgwODQy"
                   className="text-sm font-semibold text-gray-900 shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white"
                 >
                   <img
